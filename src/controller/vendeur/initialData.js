fff7ed60f0c1565b88d4be3ee757d83af7a2cf83
ali ben said <ali.bensaid@esprit.tn>
2021-11-08 12:48:09 +0000

    env change to true + json name

--- src/controller/vendeur/initialData.js
+++ src/controller/vendeur/initialData.js
@@ -213,10 +213,6 @@ exports.initialData = async (req, res) => {
     tarifs,
     myJoinRequests,
     abonnements,
-<<<<<<< Updated upstream
     livres,
-=======
-    allCommandelivre,
->>>>>>> Stashed changes
   });
 };
